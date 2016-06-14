29a2b54c5bb633ea32dc01ac40172babc94f579f
Shaun Yap <shaun@siegeflow.com>
2016-06-14 22:13:48 +0400

    Add list and todo components & test suite

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the house'
+        },
+        {
+          id: 3,
+          text: 'Feed the cats'
+        },
+        {
+          id: 4,
+          text: 'Finish Udemy Course'
+        }
+      ]
+    }
+  },
   render: function() {
+    var {todos} = this.state;
+
     return (
       <div>
-        <h1>Todo App.jsx</h1>
+        <TodoList todos={todos}/>
       </div>
     )
   }
