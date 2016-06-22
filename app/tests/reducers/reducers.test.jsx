56b39eca37f34d0be4c14d15ef1b902bade367d2
Shaun Yap <shaun@siegeflow.com>
2016-06-22 08:12:47 +0400

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,37 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      }
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggleTodo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Thing that needs done',
+        completed: true,
+        createdAt: 123,
+        completedAt: 300
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].compeltedAt).toEqual(undefined);
+
+
+    });
+  });
 });
