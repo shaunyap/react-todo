add45337835d7294be74bebe37cfed81602ba898
Shaun Yap <shaunyap@gmail.com>
2016-06-28 08:04:57 +0400

    Add login page and firebase actions

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-import firebase, {firebaseRef} from 'app/firebase/';
+import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 
 export var setSearchText = (searchText) => {
   return {
@@ -88,4 +88,22 @@ export var startToggleTodo = (id, completed) => {
       dispatch(updateTodo(id, updates));
     });
   };
+};
+
+export var startLogin= () => {
+  return(dispatch, getState) => {
+    firebase.auth().signInWithPopup(githubProvider).then((result) => {
+      console.log('Auth worked', result);
+    }, (error) => {
+      console.log('Unable to Auth', error);
+    })
+  }
+};
+
+export var startLogout= () => {
+  return(dispatch, getState) => {
+    firebase.auth().signOut().then(() => {
+      console.log('Logged out.');
+    });
+  }
 };
